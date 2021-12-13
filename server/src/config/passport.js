6755f3bf7dbb1ab210505fdfb1e919efda66dda2
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-12-13 12:11:44 +0100

    add server

---
+++ server/src/config/passport.js
@@ -0,0 +1,67 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/User')
+
+
+passport.use(
+    new LocalStrategy({ 
+        usernameField : 'email', 
+        passwordField : 'password', 
+        passReqToCallback : true 
+    }, async (req, email, password, done) => {
+        const user = await User.findOne({email: email})
+        if(!user) {
+            return done(null, false, {message: "User not found"})
+        } else {
+            const match = await user.comparePassword(password)
+            if(match){
+                return done(null, user)
+            } else {
+                return done(null, false, {message: "incorrect password"})
+            }
+        }
+    })
+);
+
+//almaceno en sesion el id del usuario
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user)
+    })
+})
+// student
+
+// passport.use(
+//     new LocalStrategy({ 
+//         usernameField : 'email', 
+//         passwordField : 'password', 
+//         passReqToCallback : true 
+//     }, async (req, email, password, done) => {
+//         const student = await Student.findOne({email: email})
+//         if(!student) {
+//             return done(null, false, {message: "student not found"})
+//         } else {
+//             const match = await student.comparePassword(password)
+//             if(match){
+//                 return done(null, student)
+//             } else {
+//                 return done(null, false, {message: "incorrect password"})
+//             }
+//         }
+//     })
+// );
+
+// //almaceno en sesion el id del usuario
+// passport.serializeUser((student, done) => {
+//     done(null, student.id)
+// })
+
+// passport.deserializeUser((id, done) => {
+//     User.findById(id, (err, user) => {
+//         done(err, user)
+//     })
+// })
