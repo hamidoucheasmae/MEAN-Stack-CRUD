7402096136d78669d989286fbf3ab7115fe2d15d
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-12-16 16:20:14 +0100

    update edit

--- frontend/src/app/_components/member-details/member-details.component.ts
+++ frontend/src/app/_components/member-details/member-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/models/member.model';
+import { MemberService } from 'src/app/_services/member.service';
+import { UserService } from 'src/app/_services/user.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-member-details',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-details.component.css']
 })
 export class MemberDetailsComponent implements OnInit {
+  members?: Member[];
+  currentUser: any;
+  currentIndex = -1;
+  username = '';
 
-  constructor() { }
 
-  ngOnInit() {
+
+
+  constructor(private userService: UserService, private tokenStorageService: TokenStorageService, private router : Router,private memberService:MemberService) { }
+
+  ngOnInit(): void {
+    this.retrieveMembers();
+
+    this.currentUser = this.tokenStorageService.getUser();
+
+    this.userService.getAdminBoard().subscribe(
+      data => {
+        this.members = data;
+      },
+      err => {
+        this.router.navigate(['/login'])
+         return false
+      }
+    );
+  }
+
+  retrieveMembers(): void {
+    this.memberService.getAll()
+      .subscribe(
+        data => {
+          this.members = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  refreshList(): void {
+    this.retrieveMembers();
+    this.currentUser = {};
+    this.currentIndex = -1;
+  }
+  setActiveMember(member: Member, index: number): void {
+    this.currentUser = member;
+    this.currentIndex = index;
   }
 
 }
